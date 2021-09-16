932dd88bddc376642bd25b42786ecff0f5e4c84c
Adrian Wawryczuk <adrian.wawryczuk@gmail.com>
2021-09-16 23:30:53 +0200

    Heroku conf

--- client/service/apolloClient.ts
+++ client/service/apolloClient.ts
@@ -14,7 +14,7 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined', // set to true for SSR
     link: new HttpLink({
-      uri: process.env.GRAPHQLURL ?? 'http://localhost:4000/graphql',
+      uri: 'https://next-sonepar-web-api2.herokuapp.com/graphql',
     }),
     cache: new InMemoryCache(),
   })
